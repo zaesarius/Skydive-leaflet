a062ebed06b8083831069948eeaac65d1e7df246
zaesar <zaesarpo@gmail.com>
2018-11-23 12:43:56 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/10eMJLXLywy1flLAAmFi-cIAB6VT2Qnh58T0orkDKyZ4/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/19u9Ba9wUkpDxAMBHQvQ1EKkrys58H5rvDCprGVUaXyc/edit#gid=0';
