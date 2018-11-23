59649535d418288ba1f64dc7851158522fe911ab
zaesar <zaesarpo@gmail.com>
2018-11-23 11:30:12 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1LyDr4GxBrwVzmNcF4iJjXSAW4mTjxdKIgiKf06z-kOA/edit#gid=0';
