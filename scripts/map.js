5c5d346984157e308fa1e6b84adc93d4d99e2844
zaesar <zaesarpo@gmail.com>
2018-12-10 11:16:33 +0100

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -1,3 +1,17 @@
+var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+});,
+    satelital   = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
+
+var map = L.map('map', {
+    layers: [street]
+});
+
+L.control.layers().addTo(map);
+
+
 $(window).on('load', function() {
   var documentSettings = {};
   var markerColors = [];
